6611dbae6d49fb8bb7e063d0b5ce9b009e8eeead
shashidhar jonchhe <shashi@shashidhars-MacBook-Pro.local>
2020-06-02 15:37:13 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component{
+  constructor(){
+    super()
+      this.state={
+        name:[]
+      }
+  }
+
+  componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/users')
+
+          .then(res=>{
+            const name = res.data;
+            this.setState({
+               name
+            })
+          }) 
+  }
+
+
+  render(){
+    return(
+      <div className='App'>
+            <ul>
+              {
+                this.state.name.map(el=><li>{el.name}</li>)
+              }
+            </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
